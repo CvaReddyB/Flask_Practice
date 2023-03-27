f8d5b73dbad4a0c16ee1d412504a828dcf583762
Siva Prasad <vspreddy378@gmail.com>
2023-03-27 23:23:15 +0530

    Added user list

--- script.py
+++ script.py
@@ -5,7 +5,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return {"app": "flask app", "version": "1.0.0"}
+    return {
+        "app": "flask app",
+        "version": "1.0.0",
+    }
+
+
+@app.route("/user/")
+def user_list():
+    return {
+        "app": "flask app",
+        "version": "1.0.0",
+        "name": "users",
+    }
 
 
 app.run(debug=True)
