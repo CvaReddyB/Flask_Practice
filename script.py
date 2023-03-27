4ad08e194425a6da7be2deebd4bf7a2bced4ca4a
Siva Prasad <vspreddy378@gmail.com>
2023-03-27 23:32:04 +0530

    added data passsing

--- script.py
+++ script.py
@@ -1,4 +1,6 @@
-from flask import Flask
+import os
+from pathlib import Path
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -17,6 +19,47 @@ def user_list():
         "app": "flask app",
         "version": "1.0.0",
         "name": "users",
+    }
+
+@app.route("/data_passing/")
+def data_passing():
+    headers = dict(request.headers)
+    form = dict(request.form)
+
+    files = request.files
+    file = files.get("profile")
+
+    if file:
+        uploads = Path("uploads")
+        os.makedirs(uploads, exist_ok=True)
+        file.save(uploads / file.filename)
+        file = {
+            "name": file.name,
+            "filename": file.filename,
+            "mimetype": file.mimetype,
+            "headers": dict(file.headers),
+            "size": os.stat(uploads / file.filename).st_size,
+            "content_type": file.content_type,
+        }
+
+    try:
+        json = dict(request.json)
+    except BaseException as e:
+        json = str(e)
+
+    data = request.get_data(as_text=True)
+    args = dict(request.args)
+    return {
+        "app": "my flask app - users",
+        "version": "1.0.0",
+        "headers": headers,
+        "src-Lang": request.headers.get("src-Lang"),
+        "Src-Lang": request.headers.getlist("Src-Lang"),
+        "form": form,
+        "json": json,
+        "data": data,
+        "args": args,
+        "file": file,
     }
 
 
