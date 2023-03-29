5ac11802810d5705ee90366e1444b9782b586726
Siva Prasad <vspreddy378@gmail.com>
2023-03-29 23:38:08 +0530

    accept_file_process_data

--- script.py
+++ script.py
@@ -21,6 +21,7 @@ def user_list():
         "name": "users",
     }
 
+
 @app.route("/data_passing/")
 def data_passing():
     headers = dict(request.headers)
@@ -60,6 +61,57 @@ def data_passing():
         "data": data,
         "args": args,
         "file": file,
+    }
+
+
+"""
+- - - line count
+- - - word count (total & per line)
+- - - unique words & occurrence
+- - - unique chars & occurrence
+"""
+
+
+@app.route("/accept_file_process_data/")
+def accept_file_process_data():
+    data = request.files
+    data_ = [line.decode() for line in data["filepath"]]
+    no_of_lines = str(len(data_))
+    unique_word_dict = {}
+    total_word_count = 0
+    lines_wordcount = []
+    for no, line in enumerate(data_):
+        wordlst = line.split()
+        line_count = len(wordlst)
+        total_word_count += line_count
+        for word in wordlst:
+            if word not in unique_word_dict:
+                unique_word_dict[word] = 1
+            else:
+                unique_word_dict[word] += 1
+        # print(f"Lines and its word count ::: {line}\t{line_count}")
+        print(type(line))
+        lines_wordcount.append(line_count)
+
+    char_dict = {}
+    for line in data_:
+        for char in line:
+            if char not in char_dict:
+                char_dict[char] = 1
+            else:
+                char_dict[char] += 1
+
+    print(f"No of lines in file ::: {no_of_lines}")
+    print(f"Total No of words in file ::: {total_word_count}")
+    print(f"Total No of Uniq words in file ::: {len(unique_word_dict)}")
+    print(f"Total No of Uniq chars in file ::: {len(char_dict)}")
+
+    return {
+        "no_of_lines": no_of_lines,
+        "total_word_count": total_word_count,
+        "lines_wordcount": lines_wordcount,
+        "unique_word_dict": unique_word_dict,
+        "char_dict": char_dict,
     }
 
 
